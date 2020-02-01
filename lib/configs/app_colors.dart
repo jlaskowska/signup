271be9e7aad550709d57570f768fae9a1c008eb9
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-02-01 17:37:55 +0100

    Added AppColors.

---
+++ lib/configs/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const purple = const Color(0xff8b4d7e);
+  static const pink = const Color(0xfffd9298);
+  static const yellow = const Color(0xffe5dc7d);
+  static get white => Colors.white;
+  static const gray = const Color(0xff8c8c8c);
+  static const black = const Color(0xff383838);
+  static const red = const Color(0xff772233);
+}
