8f6da45e5fe0146ad5f8cd02c9ff200423b8e6ba
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-02-01 12:41:59 +0100

    Added Provider for dependency injection.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:signup/services/firebase_auth_service.dart';
+import 'package:signup/services/i_auth_service.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
+    return MultiProvider(
+      providers: [
+        Provider<IAuthService>(
+          create: (_) => FirebaseAuthService(),
+        )
+      ],
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Welcome to Flutter'),
+          ),
+          body: Center(
+            child: Text('Hello World'),
+          ),
         ),
       ),
     );
