d06d5fd9e240dbc8b3d415e0224eba342807b3bd
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-02-05 18:26:18 +0100

    Added UI for SigninScreen.

--- lib/widgets/signin_screen/signin_screen.dart
+++ lib/widgets/signin_screen/signin_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:signup/configs/app_colors.dart';
+import 'package:signup/widgets/common/custom_text_field.dart';
+import 'package:signup/widgets/signin_screen/signin_screen_background.dart';
 
 class SigninScreen extends StatelessWidget {
   const SigninScreen({Key key}) : super(key: key);
@@ -6,10 +9,88 @@ class SigninScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          child: Center(
-            child: Text('Signin Screen'),
+      body: SigninScreenBackground(
+        child: SafeArea(
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Spacer(flex: 1),
+                Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Text(
+                      'Welcome\nBack',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 40),
+                        child: CustomTextField.dark(
+                          onChanged: null,
+                          labelText: 'Email',
+                          hintText: 'Email',
+                          errorText: null,
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 40),
+                        child: CustomTextField.dark(
+                          onChanged: null,
+                          labelText: 'Password',
+                          hintText: 'Password',
+                          errorText: null,
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(
+                        'Sign In',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      RawMaterialButton(
+                        onPressed: () {},
+                        child: Icon(
+                          Icons.arrow_forward,
+                          color: AppColors.white,
+                          size: 35.0,
+                        ),
+                        shape: CircleBorder(),
+                        elevation: 2.0,
+                        fillColor: AppColors.purple,
+                        padding: const EdgeInsets.all(8.0),
+                        constraints: BoxConstraints(
+                          minHeight: 75,
+                          minWidth: 75,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/signin_screen/signin_screen_background.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'package:signup/configs/app_colors.dart';
+
+class SigninScreenBackground extends StatelessWidget {
+  final Widget child;
+
+  const SigninScreenBackground({
+    @required this.child,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: CustomPaint(
+        painter: _ShapePainter(),
+        child: child,
+      ),
+    );
+  }
+}
+
+class _ShapePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    _bottom(canvas, size);
+    _top(canvas, size); // hero animation?
+  }
+
+  void _top(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = AppColors.purple;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2.0;
+
+    var path = Path();
+    path.moveTo(0, size.height * 0.35);
+    path.quadraticBezierTo(size.width * 0.4, size.height * 0.45, size.width * 0.5, size.height * 0.35);
+    path.quadraticBezierTo(size.width * 0.7, size.height * 0.20, size.width, size.height * 0.15);
+
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    canvas.drawPath(path, paint);
+  }
+
+  void _bottom(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = AppColors.yellow;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2.0;
+
+    var path = Path();
+    path.moveTo(size.width * 0.45, size.height * 0.35);
+    path.quadraticBezierTo(size.width * 0.7, size.height * 0.5, size.width, size.height * 0.5);
+
+    path.lineTo(size.width, size.height * 0.15);
+    path.lineTo(0, 0);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
